abe14ff57dc54c44243221d5b145256fc764da46
dominicddl <e1398293@u.nus.edu>
2026-04-01 22:34:48 +0800

    feat: open onboarding page on first install

--- extension/entrypoints/background.ts
+++ extension/entrypoints/background.ts
@@ -19,8 +19,10 @@ export default defineBackground({
       }
     );
 
-    chrome.runtime.onInstalled.addListener(() => {
-      console.log('[bubb] Extension installed');
+    chrome.runtime.onInstalled.addListener((details) => {
+      if (details.reason === 'install') {
+        chrome.tabs.create({ url: chrome.runtime.getURL('/onboarding.html') });
+      }
     });
 
     chrome.runtime.onConnect.addListener((port) => {
